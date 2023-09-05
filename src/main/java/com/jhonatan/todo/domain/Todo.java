afcdea9f67a06f866dc4c5495bd9292041e4ddb7
Jhonatan Dinz <jhonatangomesdiniz@gmail.com>
2023-09-05 08:18:33 -0300

    Criado classe de domain ToDo

---
+++ src/main/java/com/jhonatan/todo/domain/Todo.java
@@ -0,0 +1,93 @@
+package com.jhonatan.todo.domain;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String titulo;
+	private String descricao;
+	private LocalDateTime dtFinalizar;
+	private boolean finalizado = false;
+
+	public Todo() {
+		super();
+	}
+
+	public Todo(Long id, String titulo, String descricao, LocalDateTime dtFinalizar, boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.dtFinalizar = dtFinalizar;
+		this.finalizado = finalizado;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getDtFinalizar() {
+		return dtFinalizar;
+	}
+
+	public void setDtFinalizar(LocalDateTime dtFinalizar) {
+		this.dtFinalizar = dtFinalizar;
+	}
+
+	public boolean isFinalizado() {
+		return finalizado;
+	}
+
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Todo other = (Todo) obj;
+		return Objects.equals(id, other.id);
+	}
+}
